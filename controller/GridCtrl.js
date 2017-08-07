165a35209def01a45a2654745a842fba3d18c721
qiupenghua1994 <qiupenghua1994@163.com>
2017-08-07 20:34:39 +0800

    Initial commit

---
+++ controller/GridCtrl.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Administrator on 2017/7/28.
+ */
+(function (angular) {
+
+
+   function GridController($scope){
+
+      $scope.clearFormDate= function () {
+         App.utils.clearFormDate($scope.first);
+      };
+      var data =[
+         {no:'1',name:'笑傲王'},
+         {no:'2',name:'对对对'},
+         {no:'2',name:'对对对'},
+         {no:'2',name:'对对对'},
+         {no:'2',name:'对对对'},
+      ];
+      $scope.data1=1;
+      $scope.grid = {
+         queryUrl:'test',
+         onGridReady: function () {
+            $scope.grid.api.reloadData();
+         },
+         pageSize:20,
+         onRowSelected: function (data) {
+            debugger
+         },
+         columnDefs:[
+            {
+               headerName:'客户号',
+               field:'no'
+            },
+            {
+               headerName:'客户姓名',
+               field:'name'
+            }
+
+         ]
+      }
+   }
+
+   angular.module('paper')
+       .controller('GridController', GridController)
+
+}(window.angular));
