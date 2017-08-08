3e76ef391ab09d923c85cb6f0fb5061798bad3e8
qiupenghua1994 <qiupenghua1994@163.com>
2017-08-08 23:20:07 +0800

    登录

--- person/login/controller/LoginCtrl.js
+++ person/login/controller/LoginCtrl.js
@@ -3,21 +3,54 @@
  */
 (function (angular) {
 
+    var $cookies;
+
     function toIndex(){
         window.location.href = "/index.html";
     }
 
-    function LoginController($scope) {
+    //初始化$cookies
+    function setCookies(cook) {
+        if (!$cookies) {
+            $cookies = cook;
+        }
+    }
+
+    //设置cookie
+    var setCookie = function (name, value) {
+        if (value) {
+            $cookies.putObject(name, value, {expires: new Date(new Date().getTime() + 5000000)});
+            //$cookies.putObject(name,value);
+        } else {
+            $cookies.putObject(name, value, {expires: new Date(new Date().getTime() + 5000000)});
+        }
+    };
+    //获取cookie
+    var getCookie = function () {
+        var user = $cookies.getObject("user");
+        if (!user) {
+            return;
+        }
+        if (user.account) {
+            $scope.user = user;
+            $scope.loginClick();
+        }
+    };
+
+    function LoginController($scope, $cookies) {
+        setCookies($cookies);
+
         $scope.loginBtn = function () {
-
-
+            setCookie('user', {name: 'qph', age: 13});
+            toIndex()
         };
     }
 
-    function RegistController($scope) {
+    function RegistController($scope, $cookies) {
+        setCookies($cookies);
 
         $scope.registBtn = function () {
-
+            toIndex()
         }
     }
 
